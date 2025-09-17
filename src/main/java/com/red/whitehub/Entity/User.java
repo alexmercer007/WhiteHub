081af880de05f42e2cc6d441a4482a7a5258ef4d
Alejandro Reyes Castillo <alexmercer0072015@gmail.com>
2025-09-17 11:37:49 -0400

    Se añadieron algunos registradores de fechas y booleanos

--- src/main/java/com/red/whitehub/Entity/User.java
+++ src/main/java/com/red/whitehub/Entity/User.java
@@ -7,7 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
 @Table(name ="User")
@@ -15,7 +18,7 @@ public class User {
  
  @Id 
  @GeneratedValue( strategy = GenerationType.AUTO )
- private Long Id;
+ private Long id;
     
  @Column( name = "name")
  private String name;
@@ -26,15 +29,39 @@ public class User {
  @Column( name = "userName")
  private String userName;
  
+ @Column( name = "email")
+ private String email;
+ 
  @Column( name = "password")
  private String password;
+ 
+ @Column( name = "birthdate")
+ private LocalDate birthdate;
+ 
+ @Column( name = "birthdate")
+ private LocalDateTime createdAt;
+ 
+@Column( name = "updatedAt")
+private LocalDateTime updatedAt;
+
+@Column( name = "banned")
+private boolean banned;
+
+@Column( name = "bannedAt")
+private LocalDateTime bannedAt;
+
+@Column( name = "deleted")
+private boolean deleted;
+
+@Column( name = "deletedAt")
+private LocalDateTime deletedAt;
  
  
 //----------------------------------------------------------------------------- Getters ------------------------------------------------------------------------------------------// 
  
  
 public Long getId(){
-     return this.Id;
+     return this.id;
  } 
  
  public String getName(){
@@ -48,11 +75,42 @@ public Long getId(){
   public String getUserName(){
      return this.userName;
  }
+  
+  public String getEmail(){
+     return this.email;
+ }
    
   public String getPassword(){
      return this.password;
  }
  
+ public LocalDate getbirthdate(){
+     return this.birthdate;
+ }
+ 
+ public LocalDateTime getCreatedAt(){
+     return this.createdAt; 
+ }
+
+ public LocalDateTime getUpdatedAt(){
+     return this.updatedAt; 
+ }
+
+ public Boolean getBanned(){
+     return this.banned; 
+ }
+
+ public LocalDateTime getBannedAt(){
+     return this.bannedAt;
+ }
+
+ public Boolean getDeleted(){
+     return this.deleted;
+ }
+
+ public LocalDateTime getDeletedAt(){
+     return this.deletedAt; 
+ } 
  
  
  
@@ -70,14 +128,45 @@ public Long getId(){
   public void setUserName( String userName ){
      this.userName = userName;
  }
+  
+  public void setEmail( String email){
+      this.email = email;
+ }
    
-  public void getPassword( String password ){
-      this.password = password;
+  public void setPassword( String password ){
+      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+      String encoded = encoder.encode(password);     
+    this.password = encoded;
  }
  
+ public void setbirthdate( LocalDate birthdate ){
+      this.birthdate = birthdate;
+ }
  
+  public void setCreatedAt( LocalDateTime createdAt ){
+     this.createdAt = createdAt; 
+ }
+
+ public void setUpdatedAt( LocalDateTime updatedAt ){
+     this.updatedAt = updatedAt; 
+ }
+
+ public void setBanned( Boolean banned ){
+     this.banned = banned; 
+ }
+
+ public void setBannedAt( LocalDateTime bannedAt ){
+     this.bannedAt = bannedAt;
+ }
+
+ public void setDeleted( Boolean deleted ){ 
+     this.deleted = deleted;
+ }
+
+ public void getDeletedAt( LocalDateTime deletedAt ){
+     this.deletedAt = deletedAt; 
+ } 
  
 
- 
  
 }
